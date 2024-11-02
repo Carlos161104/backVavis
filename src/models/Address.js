c13fb886b8f49c6e4ab56230c81871de56d1486d
Gael Vivas <gaelvn2020@outlook.com>
2024-11-01 22:43:19 -0600

    added Address model

---
+++ src/models/Address.js
@@ -0,0 +1,59 @@
+import { sequelize } from "../database/database.js";
+
+export const Address = sequelize.define("addresses", {
+  id: {
+    type: type.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+
+  country: {
+    type: type.STRING(20),
+    allowNull: false,
+  },
+
+  state: {
+    type: type.STRING(20),
+    allowNull: false,
+  },
+
+  city: {
+    type: type.STRING(20),
+    allowNull: false,
+  },
+
+  postal_code: {
+    type: type.STRING(20),
+    allowNull: false,
+  },
+
+  address_line_1: {
+    type: type.STRING(20),
+    allowNull: false,
+  },
+
+  address_line_2: {
+    type: type.STRING(20),
+    allowNull: true,
+  },
+
+  address_line_3: {
+    type: type.STRING(20),
+    allowNull: true,
+  },
+
+  comments: {
+    type: type.STRING(30),
+    allowNull: true,
+  },
+
+  channel_id: {
+    type: type.STRING(50),
+    allowNull: false,
+  },
+
+  client_id: {
+    type: type.INTEGER,
+    allowNull: false,
+  },
+});
